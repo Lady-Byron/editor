0897b21de40ce3df1691d0d0609f2ec50acac0d0
Lady-Byron <samantha-everett@outlook.com>
2026-01-18 17:03:21 -0500

    Update index.ts

--- js/src/forum/extensions/index.ts
+++ js/src/forum/extensions/index.ts
@@ -2,3 +2,5 @@ export { SpoilerInline } from './SpoilerInline';
 export { SpoilerInlineParagraph } from './SpoilerInlineParagraph';
 export { SpoilerBlock } from './SpoilerBlock';
 export { SubscriptMark, SuperscriptMark } from './SubSuperscript';
+export { BlankLine } from './BlankLine';
+export { LbIndent } from './LbIndent';
