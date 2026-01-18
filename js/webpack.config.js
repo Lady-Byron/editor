4b8e7cc87875e1dd2559bd6d568e370dc4b682fd
Lady-Byron <samantha-everett@outlook.com>
2026-01-17 22:03:43 -0500

    Update webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,3 +1,17 @@
 const config = require('flarum-webpack-config');
+const path = require('path');
 
-module.exports = config();
+const baseConfig = config();
+
+module.exports = {
+    ...baseConfig,
+    resolve: {
+        ...baseConfig.resolve,
+        alias: {
+            ...baseConfig.resolve?.alias,
+            // 强制 marked 解析到项目的 node_modules/marked，
+            // 解决 @tiptap/markdown 内置旧版 Marked 的 em/strong 解析 bug
+            'marked': path.resolve(__dirname, 'node_modules/marked'),
+        },
+    },
+};
