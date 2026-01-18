e46289fb498c3d9965ce68922b0df398ff6a0fa3
Lady-Byron <samantha-everett@outlook.com>
2026-01-17 22:16:30 -0500

    Update webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,17 +1,16 @@
-const config = require('flarum-webpack-config');
-const path = require('path');
+const config = require('flarum-webpack-config')
+const path = require('path')
 
-const baseConfig = config();
+const baseConfig = config()
 
 module.exports = {
-    ...baseConfig,
-    resolve: {
-        ...baseConfig.resolve,
-        alias: {
-            ...baseConfig.resolve?.alias,
-            // 强制 marked 解析到项目的 node_modules/marked，
-            // 解决 @tiptap/markdown 内置旧版 Marked 的 em/strong 解析 bug
-            'marked': path.resolve(__dirname, 'node_modules/marked'),
-        },
+  ...baseConfig,
+  resolve: {
+    ...baseConfig.resolve,
+    alias: {
+      ...(baseConfig.resolve?.alias || {}),
+      // 关键：强制使用 ESM 版本，确保存在 named exports: marked / Marked
+      'marked$': path.resolve(__dirname, 'node_modules/marked/lib/marked.esm.js'),
     },
-};
+  },
+}
