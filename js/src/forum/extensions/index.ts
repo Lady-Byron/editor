d6b682216eead04ddc06590c48279fb60b9f98b7
Lady-Byron <samantha-everett@outlook.com>
2026-01-25 20:19:36 -0500

    Update index.ts

--- js/src/forum/extensions/index.ts
+++ js/src/forum/extensions/index.ts
@@ -8,5 +8,4 @@ export { CustomLink } from './CustomLink';
 export { AlignedBlock } from './AlignedBlock';
 export { TextColor } from './TextColor';
 export { TextSize } from './TextSize';
-export { getLbInlineTokens, getLbBlockTokens } from './markedHelper';
 export type { TextAlignment } from './AlignedBlock';
